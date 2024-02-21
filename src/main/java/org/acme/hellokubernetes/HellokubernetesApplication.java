3d7a248cc916cb17585930efcd64b9d9a0619df3
wael2000 <weldoami@redhat.com>
2024-02-21 23:04:01 +0100

    Welcome to Spring Boot & Kubernetes Workshop

---
+++ src/main/java/org/acme/hellokubernetes/HellokubernetesApplication.java
@@ -0,0 +1,13 @@
+package org.acme.hellokubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellokubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellokubernetesApplication.class, args);
+	}
+
+}
